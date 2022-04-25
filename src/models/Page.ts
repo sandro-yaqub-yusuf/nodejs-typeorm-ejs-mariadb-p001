d34a6c1ddf6fc355a96388057bc05fc0cc8b7154
Sandro Yaqub Yusuf <sandroyy@gmail.com>
2022-04-25 15:01:55 -0300

    Implementações no projeto
    Melhorias no projeto

--- src/models/Page.ts
+++ src/models/Page.ts
@@ -5,7 +5,7 @@ export default class Page {
     @typeorm.PrimaryGeneratedColumn({ type: 'int', name: 'id', unsigned: true })
     id: number;
 
-    @typeorm.Column('varchar', { name: 'name', length: 50 })
+    @typeorm.Column('varchar', { name: 'name', length: 25 })
     name: string;
 
     @typeorm.Column('text', { name: 'content', nullable: true })
--- src/routes/adminPageRoutes.ts
+++ src/routes/adminPageRoutes.ts
@@ -1,18 +1,15 @@
 import express from 'express';
-import multer from 'multer';
-import uploadConfig from '../configs/upload';
 import * as PageValidator from '../validators/pageValidator';
 import AdminPageController from '../controllers/adminPageController';
 import AuthMiddleware from '../middlewares/authMiddleware';
 
 const adminPageRouter = express.Router();
-const upload = multer(uploadConfig);
 
 adminPageRouter.get('/listar', AuthMiddleware, AdminPageController.index);
 adminPageRouter.get('/novo', AuthMiddleware, AdminPageController.create);
-adminPageRouter.post('/novo', AuthMiddleware, upload.single('customFile'), PageValidator.store, AdminPageController.store);
+adminPageRouter.post('/novo', AuthMiddleware, PageValidator.store, AdminPageController.store);
 adminPageRouter.get('/editar/:id', AuthMiddleware, AdminPageController.edit);
-adminPageRouter.post('/editar/:id', AuthMiddleware, upload.single('customFile'), PageValidator.update, AdminPageController.update);
+adminPageRouter.post('/editar/:id', AuthMiddleware, PageValidator.update, AdminPageController.update);
 adminPageRouter.get('/excluir/:id', AuthMiddleware, AdminPageController.delete);
 adminPageRouter.post('/excluir/:id', AuthMiddleware, AdminPageController.destroy);
 adminPageRouter.get('/consultar/:id', AuthMiddleware, AdminPageController.show);
